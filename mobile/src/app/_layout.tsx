3b37d21114e17fd1686c5e86e9b442be6f8bb0fd
Kossi Georges-Noé AHOMBO <georges.ahombo@gmail.com>
2026-07-08 19:22:45 +0000

    chore: reset mobile project with default template instead of blank

--- mobile/src/app/_layout.tsx
+++ mobile/src/app/_layout.tsx
@@ -1,9 +1,5 @@
 import { Stack } from "expo-router";
 
-export default function AndroidLayout() {
-  return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
-  );
+export default function RootLayout() {
+  return <Stack />;
 }
